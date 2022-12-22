67ef7e9e59be5302bd6e27d48ef54a5e9cd4d8e6
Thiago Emanoel <thiagoemanoel448@gmail.com>
2022-12-22 11:11:16 -0300

    rangers

---
+++ 2 - Controles de fluxo e coleções/rangers.kt
@@ -0,0 +1,19 @@
+for(i in 0..3) {             // 3 é incluido
+    print(i)
+}
+print(" ")
+
+for(i in 0 until 3) {        // 3 não é incluido
+    print(i)
+}
+print(" ")
+
+for(i in 2..8 step 2) {      // Vai pulando de 2 em 2
+    print(i)
+}
+print(" ")
+
+for (i in 3 downTo 0) {      // Vai decrescendo
+    print(i)
+}
+print(" ")
